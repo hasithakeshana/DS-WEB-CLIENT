074fb52683ae143b5d03279e9d99ebc77485dd40
hasitha <hasithakeshana9900@gmail.com>
2020-04-29 14:14:46 +0530

    add check active sensor function

--- src/components/sensor.js
+++ src/components/sensor.js
@@ -52,15 +52,46 @@ const Sensor = (props) => {
 				<br></br>
 			</MDBCol>
 		);
-	}
-
-	// sensor smoke level or co2 level not above 5 this segment executed
-	else {
+	} else if (!item.active) {
+		// check sensor is active or not
+		return (
+			<MDBCol lg="4" md="12" className="mb-lg-0 mb-4">
+				<MDBCard className="green">
+					{" "}
+					{/* set color to green */}
+					<MDBCardBody className="white-text">
+						<h5 className="mb-4">sensor id : {item.id}</h5>
+						<div className="d-flex justify-content-center">
+							<div className="card-circle d-flex justify-content-center align-items-center">
+								<MDBIcon icon="users" className="light-blue-text" />
+							</div>
+						</div>
+						<h2 className="font-weight-bold my-4">
+							smoke level : {item.smokeLevel}
+						</h2>
+						<h2 className="font-weight-bold my-4">
+							co2 level : {item.co2Level}
+						</h2>
+						<h5 className="font-weight-bold my-4">location</h5>
+						<h5 className="mb-4">
+							floor no : {item.floorNo} room no : {item.roomNo}
+						</h5>
+						{item.active ? (
+							<h5 className="font-weight-bold my-4">status : active</h5>
+						) : (
+							<h5 className="font-weight-bold my-4">status : not active</h5>
+						)}
+					</MDBCardBody>
+				</MDBCard>
+				<br></br>
+			</MDBCol>
+		);
+	} else {
 		return (
 			<MDBCol lg="4" md="12" className="mb-lg-0 mb-4">
 				<MDBCard className="indigo">
 					{" "}
-					{/* set color to indigo */}
+					{/* set color to indigo if smoke level or co2 level less than 5 and active */}
 					<MDBCardBody className="white-text">
 						<h5 className="mb-4">sensor id : {item.id}</h5>
 						<div className="d-flex justify-content-center">
